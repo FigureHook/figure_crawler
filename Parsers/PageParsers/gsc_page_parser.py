622c04f5c0eed46cf132b0ee2afc715aa4627b9a
EltonChou <ie9181124@gmail.com>
2020-09-14 05:34:21 +0800

    [ADD] gsc page parser

---
+++ Parsers/PageParsers/gsc_page_parser.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+
+import requests as rq
+from bs4 import BeautifulSoup
+
+
+class GSCYearlyAnnouncement:
+    def __init__(self, category, start=2006, end=None, lang="ja"):
+        if not end:
+            end = datetime.now().year
+
+        if end < start:
+            raise ValueError
+
+        self.period = range(start, end+1)
+        self.base_url = make_base_url(category, lang)
+
+    def __iter__(self):
+        for year in self.period:
+            url = self.base_url(year)
+            response = rq.get(url)
+            page = BeautifulSoup(response.text, "lxml")
+            product_urls = (item["href"] for item in page.select(
+                ".hitItem:not(.shimeproduct) > .hitBox > a"
+                ))
+
+            yield product_urls
+
+
+def make_base_url(category, lang):
+    def maker(year):
+        base_url = "https://www.goodsmile.info/{lang}/products/category/{category}/announced/{year}".format(
+            lang=lang,
+            category=category,
+            year=year,
+        )
+
+        return base_url
+    return maker
