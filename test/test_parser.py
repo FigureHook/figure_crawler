a5bc88f7ca444ed9684b6066d40abaafa9a8f0be
EltonChou <ie9181124@gmail.com>
2020-09-15 20:22:48 +0000

    [ADD & REFINE] add alter test case & refine gsc test case

--- test/test_parser.py
+++ test/test_parser.py
@@ -1,24 +1,18 @@
 from datetime import datetime
 
 import pytest
+import yaml
 
 from Parsers import AlterProductParser, GSCProductParser
 
 from Parsers.gsc_parser import parse_locale
 
-basic_product_attr = [
-    "name", "series", "manufacturer",
-    "category", "price", "release_date",
-    "order_period", "scale", "size",
-    "sculptor", "paintwork", "resale",
-    "adult", "copyright", "releaser",
-    "distributer", "JAN", "maker_id",
-    "images",
-]
 
+def load_yaml(path):
+    with open(path, "r") as stream:
+        sth = yaml.safe_load(stream)
 
-def make_expected_item(values):
-    return dict(zip(basic_product_attr, values))
+    return sth
 
 
 class BaseTestCase:
@@ -42,8 +36,8 @@ class BaseTestCase:
         release_date = item["test"].parse_release_date()
         the_type = type(release_date)
         assert the_type is datetime or the_type is None
-        assert release_date.year == item["expected"]["release_date"][0]
-        assert release_date.month == item["expected"]["release_date"][1]
+        assert release_date.year == item["expected"]["release_date"].year
+        assert release_date.month == item["expected"]["release_date"].month
 
     def test_order_period(self, item):
         order_period = item["test"].parse_order_period()
@@ -55,8 +49,8 @@ class BaseTestCase:
 
         assert type(start) is datetime
         assert type(end) is datetime
-        assert start == datetime(*item["expected"]["order_period"][0])
-        assert end == datetime(*item["expected"]["order_period"][1])
+        assert start == item["expected"]["order_period"]["start"]
+        assert end == item["expected"]["order_period"]["end"]
 
     def test_sculptor(self, item):
         sculptor = item["test"].parse_sculptor()
@@ -114,242 +108,24 @@ class BaseTestCase:
 
 
 class TestGSCParser(BaseTestCase):
-    products = (
-        {
-            "url": "https://www.goodsmile.info/ja/product/4364",
-            "detail": (
-                "三世村正", "装甲悪鬼村正", "ウイング",
-                "フィギュア", 11800, (2016, 9, 1),
-                ((2016, 4, 25, 12, 0), (2016, 5, 25, 21, 0)), 7, 250,
-                "絵里子（新居興業）", None, True,
-                True, "©2009-2014 Nitroplus", "ウイング",
-                "グッドスマイルカンパニー", None, "4364",
-                "images.goodsmile.info/cgm/images/product/20140407/4364/28492/large/b5070189c0ebd591226eafb796fd7a77.jpg"
-            ),
-        },
-        {
-            "url": "https://www.goodsmile.info/ja/product/8978",
-            "detail": (
-                "A-Z:[B]", "A-Z:", "Myethos",
-                "フィギュア", 12545, (2020, 6, 1),
-                ((2019, 11, 14, 12, 0), (2019, 12, 18, 21, 0)), 7, 250,
-                "SunYaMing", None, False,
-                False, "©neco/A-Z:PROJECT", "Myethos",
-                "グッドスマイルカンパニー", None, "8978",
-                "images.goodsmile.info/cgm/images/product/20191113/8978/65167/large/2a08f3b686ff5cb6b26ba399aedbdf2b.jpg"
-            )
-        },
-        {
-            "url": "https://www.goodsmile.info/ja/product/9683",
-            "detail": (
-                "フォーリナー/葛飾北斎", "Fate/Grand Order", "Phat!",
-                "フィギュア", 32000, (2021, 9, 1),
-                ((2020, 6, 26, 12, 0), (2020, 8, 26, 21, 0)), 7, 280,
-                "間崎祐介", "佐倉", False,
-                False, "©TYPE-MOON / FGO PROJECT", "ファット・カンパニー",
-                "グッドスマイルカンパニー", None, "9683",
-                "images.goodsmile.info/cgm/images/product/20200624/9683/71238/large/4770e9ca6230563d478bca4b0e34f0f2.jpg"
-            )
-        },
-        {
-            "url": "https://www.goodsmile.info/ja/product/9361",
-            "detail": (
-                "セイバーオルタ 着物Ver.", "劇場版Fate/stay night [Heaven's Feel]", "ＫＡＤＯＫＡＷＡ",
-                "フィギュア", 21818, (2020, 12, 1),
-                ((2020, 3, 26, 12, 0), (2020, 6, 10, 21, 0)), 7, 275,
-                "のぶた（リボルブ）", "かわも（リボルブ）", False,
-                False, "©TYPE-MOON・ufotable・FSNPC", "KADOKAWA",
-                "グッドスマイルカンパニー", None, "9361",
-                "images.goodsmile.info/cgm/images/product/20200318/9361/68599/large/a8b96d40fe85c254fa1cdb15b32e26df.jpg"
-            )
-        },
-        {
-            "url": "https://www.goodsmile.info/ja/product/9925",
-            "detail": (
-                "二世村正", "装甲悪鬼村正", "ウイング",
-                "フィギュア", 16000, (2020, 12, 1),
-                ((2020, 8, 18, 12, 0), (2020, 9, 23, 21, 0)), 7, 240,
-                "絵里子（新居興業）", None,  False,
-                True,  "©2009-2020 Nitroplus", "ウイング",
-                "グッドスマイルカンパニー", None, "9925",
-                "images.goodsmile.info/cgm/images/product/20200814/9925/73148/large/711e1ed11101a8a6d2854a075423f568.jpg"
-            )
-        },
-        {
-            "url": "https://www.goodsmile.info/ja/product/9888",
-            "detail": (
-                "POP UP PARADE ルビー・ローズ", "RWBY(ルビー)", "グッドスマイルカンパニー",
-                "POP UP PARADE", 3545, (2021, 1, 1),
-                ((2020, 8, 4, 12, 0), (2020, 9, 30, 21, 0)), None, 170,
-                "jarel", None,  False,
-                False,  "©Rooster Teeth Productions, LLC. All Rights Reserved. Licensed by Rooster Teeth Productions, LLC", "グッドスマイルカンパニー",
-                "グッドスマイルカンパニー", None, "9888",
-                "images.goodsmile.info/cgm/images/product/20200731/9888/72837/large/b5db91fe7f646183583a07879ac280a4.jpg"
-            )
-        },
-        {
-            "url": "https://www.goodsmile.info/ja/product/9709",
-            "detail": (
-                "PREMIUM Optimus Prime（PREMIUM オプティマスプライム）", "BUMBLEBEE（バンブルビー）", "threezero",
-                "フィギュア", 60000, (2021, 6, 1),
-                ((2020, 6, 30, 12, 0), (2020, 8, 19, 21, 0)), None, 480,
-                "threezero", None,  False,
-                False,  "©2020  TOMY. © 2018 Paramount Pictures Corporation.  All Rights Reserved.\r\nTM & ® denote Japan Trademarks. Manufactured under license from TOMY Company, Ltd.\r\nFor sale in Japan only.\r\n「トランスフォーマー」「ＴＲＡＮＳＦＯＲＭＥＲＳ」は株式会社タカラトミーの登録商標です。", "threezero",
-                "グッドスマイルカンパニー", None, "9709",
-                "images.goodsmile.info/cgm/images/product/20200626/9709/71468/large/10488b14834fbb06a017d67719274723.jpg"
-            )
-        },
-        {
-            "url": "https://www.goodsmile.info/ja/product/10021",
-            "detail": (
-                "POP UP PARADE エドワード・エルリック", "鋼の錬金術師 FULLMETAL ALCHEMIST", "グッドスマイルカンパニー",
-                "POP UP PARADE", 3545, (2021, 1, 1),
-                ((2020, 9, 10, 12, 0), (2020, 10, 7, 21, 0)), None, 155,
-                None, None,  False,
-                False,  "©荒川弘／鋼の錬金術師製作委員会・MBS", "グッドスマイルカンパニー",
-                "グッドスマイルカンパニー", None, "10021",
-                "images.goodsmile.info/cgm/images/product/20200907/10021/74006/large/e1b9bd1473d3b1a6718e271d9bfad747.jpg"
-            )
-        },
-        {
-            "url": "https://www.goodsmile.info/ja/product/2171",
-            "detail": (
-                "九重りん　体操服ver.", "こどものじかん", "FREEing",
-                "フィギュア", 11238, (2009, 1, 1),
-                (None, None), 4, 190,
-                "FREEing", None,  False,
-                False,  "© 私屋カヲル／こどものじかん製作委員会", "FREEing",
-                "グッドスマイルカンパニー", None, "2171",
-                "images.goodsmile.info/cgm/images/product/20091023/2171/5942/large/e53693699d03e0847b42946c88d62f20.jpg"
-            )
-        },
-        {
-            "url": "https://www.goodsmile.info/ja/product/2138",
-            "detail": (
-                "両儀 式", "空の境界", "グッドスマイルカンパニー",
-                "フィギュア", 5238, (2009, 2, 1),
-                (None, None), 6, None,
-                "川畑和男・工藤祐司（マックスファクトリー）", None,  False,
-                False,  "© 奈須きのこ/講談社・アニプレックス・ノーツ・ufotable", "グッドスマイルカンパニー",
-                "グッドスマイルカンパニー", None, "2138",
-                "images.goodsmile.info/cgm/images/product/20091023/2138/5767/large/54d780148032d652cac51bbc02e9b443.jpg"
-            )
-        }
-    )
+    products = load_yaml("test/test_case/gsc_products.yml")
 
     @pytest.fixture(scope="class", params=products)
     def item(self, request):
         return {
             "test": GSCProductParser(request.param["url"]),
-            "expected": make_expected_item(request.param["detail"])
+            "expected": request.param
         }
 
 
 class TestAlterParser(BaseTestCase):
-    products = (
-        {
-            "url": "http://www.alter-web.jp/products/261/",
-            "detail": (
-                "アルターエゴ／メルトリリス", "Fate/Grand Order", "アルター",
-                "フィギュア", 24800, (2020, 11, 1),
-                (None, None), 8, 370,
-                "みさいる", "星名詠美", False,
-                False, "© TYPE-MOON / FGO PROJECT", "アルター",
-                None, None, "261",
-                "http://www.alter-web.jp/uploads/products/20191218134339_UP8oL6Su.jpg"
-            ),
-        },
-        {
-            "url": "http://www.alter-web.jp/products/119/",
-            "detail": (
-                "式波・アスカ・ラングレー　ジャージVer.", "ヱヴァンゲリヲン新劇場版：Q", "アルター",
-                "フィギュア", 12800, (2019, 10, 1),
-                (None, None), 7, 230,
-                "のぶた（リボルブ）", "鉄森七方", True,
-                False, "© カラー", "アルター",
-                "EVANGELION STORE(EVA GLOBAL)　日本国内", None, "119",
-                "http://www.alter-web.jp/uploads/products/20161029191822_Os9P8MSM.jpg"
-            ),
-        },
-        {
-            "url": "http://www.alter-web.jp/products/477/",
-            "detail": (
-                "モモ・ベリア・デビルーク　-ベビードール Ver.-", "To LOVEる -とらぶる- ダークネス", "アルター",
-                "フィギュア", 14400, (2021, 3, 1),
-                ((2020, 5, 25), (2020, 7, 6)), 6, 240,
-                "竜人", "星名詠美", False,
-                False, "© 矢吹健太朗・長谷見沙貴／集英社・とらぶるダークネス製作委員会", "リューノス",
-                "あみあみ", None, "477",
-                "http://www.alter-web.jp/uploads/products/20200326182117_QaB4o3cf.jpg"
-            ),
-        },
-        {
-            "url": "http://www.alter-web.jp/products/22/",
-            "detail": (
-                "柳生十兵衛　ファイナルブライドVer.", "百花繚乱", "アルター",
-                "フィギュア", 15984, (2017, 7, 1),
-                ((2016, 8, 25), (2016, 10, 11)), 8, 270,
-                "本宮あまと", "渡邊恭大", False,
-                False, "© すずきあきら・Niθ／ホビージャパン", "ホビージャパン",
-                None, None, "22",
-                "http://www.alter-web.jp/uploads/products/20161021213511_O8HI6aTN.jpg"
-            ),
-        },
-        {
-            "url": "http://www.alter-web.jp/products/264/",
-            "detail": (
-                "プリンツ・オイゲン", "アズールレーン", "アルター",
-                "フィギュア", 37800, (2021, 4, 1),
-                (None, None), 7, 270,
-                "田中冬志", ("渡邊恭大", "山本洋平", "みうらおさみ"), False,
-                False, "© 2017 Manjuu Co.,Ltd. & Yongshi Co.,Ltd. All Rights Reserved.　© 2017 Yostar, Inc. All Rights Reserved.", "アルター",
-                None, None, "264",
-                "http://www.alter-web.jp/uploads/products/20200114115725_TNP443Nj.jpg"
-            ),
-        },
-        {
-            "url": "http://www.alter-web.jp/products/264/",
-            "detail": (
-                "プリンツ・オイゲン", "アズールレーン", "アルター",
-                "フィギュア", 37800, (2021, 4, 1),
-                (None, None), 7, 270,
-                "田中冬志", ("渡邊恭大", "山本洋平", "みうらおさみ"), False,
-                False, "© 2017 Manjuu Co.,Ltd. & Yongshi Co.,Ltd. All Rights Reserved.　© 2017 Yostar, Inc. All Rights Reserved.", "アルター",
-                None, None, "264",
-                "http://www.alter-web.jp/uploads/products/20200114115725_TNP443Nj.jpg"
-            ),
-        },
-        {
-            "url": "http://www.alter-web.jp/products/181/",
-            "detail": (
-                "パープルハート", "超次元ゲイム ネプテューヌ", "アルター",
-                "フィギュア", 17800, (2018, 4, 1),
-                (None, None), 7, 350,
-                ("槙尾宗利", "ウサギ"), "DUTCH", False,
-                False, "© 2013 アイディアファクトリー・コンパイルハート/ネプテューヌ製作委員会", "アルター",
-                None, None, "181",
-                "http://www.alter-web.jp/uploads/products/20161201151222_09EtY0sl.jpg"
-            ),
-        },
-        {
-            "url": "http://www.alter-web.jp/products/196/",
-            "detail": (
-                "グウェンドリン　レイヴスラシルVer.", "オーディンスフィア レイヴスラシル", "アルター",
-                "フィギュア", 16800, (2018, 8, 1),
-                (None, None), 8, 240,
-                "i-con（藍色空色）", "星名詠美", False,
-                False, "©ATLUS ©SEGA All rights reserved.", "アルター",
-                None, None, "196",
-                "http://www.alter-web.jp/uploads/products/20170613114935_PUC7qKIW.jpg"
-            ),
-        },
-    )
+    products = load_yaml("test/test_case/alter_products.yml")
 
     @pytest.fixture(scope="class", params=products)
     def item(self, request):
         return {
             "test": AlterProductParser(request.param["url"]),
-            "expected": make_expected_item(request.param["detail"])
+            "expected": request.param
         }
 
 class TestParserUtils:
