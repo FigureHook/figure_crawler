f480e210d93c3765ce808132abb152e3d77f3f2d
EltonChou <ie9181124@gmail.com>
2020-11-27 08:54:14 +0000

    [ADD] database module

---
+++ src/database.py
@@ -0,0 +1,108 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from src.extension import engine
+from sqlalchemy import Column, Integer
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+Base = declarative_base()
+
+"""
+https://github.com/sqlalchemy/sqlalchemy/wiki/UniqueObject
+"""
+
+
+def _unique(session, cls, hashfunc, queryfunc, constructor, arg, kw):
+    cache = getattr(session, "_unique_cache", None)
+    if cache is None:
+        session._unique_cache = cache = {}
+
+    key = (cls, hashfunc(*arg, **kw))
+    if key in cache:
+        return cache[key]
+    else:
+        with session.no_autoflush:
+            q = session.query(cls)
+            q = queryfunc(q, *arg, **kw)
+            obj = q.first()
+            if not obj:
+                obj = constructor(*arg, **kw)
+                session.add(obj)
+        cache[key] = obj
+        return obj
+
+
+class UniqueMixin(object):
+    @classmethod
+    def unique_hash(cls, *arg, **kw):
+        raise NotImplementedError()
+
+    @classmethod
+    def unique_filter(cls, query, *arg, **kw):
+        raise NotImplementedError()
+
+    @classmethod
+    def as_unique(cls, session, *arg, **kw):
+        return _unique(
+            session,
+            cls,
+            cls.unique_hash,
+            cls.unique_filter,
+            cls,
+            arg, kw
+        )
+
+
+class CRUDMixin(object):
+    """Mixin that adds convenience methods for CRUD (create, read, update, delete) operations."""
+
+    @classmethod
+    def create(cls, **kwargs):
+        """Create a new record and save it the database."""
+        instance = cls(**kwargs)
+        return instance.save()
+
+    def update(self, commit=True, **kwargs):
+        """Update specific fields of a record."""
+        for attr, value in kwargs.items():
+            setattr(self, attr, value)
+        return commit and self.save() or self
+
+    def save(self, commit=True):
+        """Save the record."""
+        session.add(self)
+        if commit:
+            session.commit()
+        return self
+
+    def delete(self, commit=True):
+        """Remove the record from the database."""
+        session.delete(self)
+        return commit and session.commit()
+
+
+class Model(CRUDMixin, Base):
+    """Base model class that includes CRUD convenience methods."""
+
+    __abstract__ = True
+
+
+class PkModel(Model):
+    """Base model class that includes CRUD convenience methods, plus adds a 'primary key' column named ``id``."""
+
+    __abstract__ = True
+    id = Column(Integer, primary_key=True, autoincrement=True)
+
+    @classmethod
+    def get_by_id(cls, record_id):
+        """Get record by ID."""
+        if any(
+            (
+                isinstance(record_id, (str, bytes)) and record_id.isdigit(),
+                isinstance(record_id, (int, float)),
+            )
+        ):
+            return cls.query.get(int(record_id))
+        return None
