473258d543aba807abf4e641afc1821436fad519
Elton Chou <plscd748@gmail.com>
2021-04-12 07:46:00 +0800

    [REFINE]refined type hint and error catch in price_parse

--- src/utils/text_parser.py
+++ src/utils/text_parser.py
@@ -1,28 +1,32 @@
 import re
+from typing import Union
 
 
-def price_parse(text: str, remove_tax: bool = False) -> int:
+def price_parse(text: str, remove_tax: bool = False) -> Union[int, None]:
     pattern = r"\d+"
     price_text = ""
 
     for n in re.findall(pattern, text):
         price_text += n
 
-    price = int(price_text)
-    if remove_tax:
-        price = round(price / 1.1)
+    try:
+        price = int(price_text)
+        if remove_tax:
+            price = round(price / 1.1)
+    except ValueError:
+        return None
 
     return price
 
 
-def scale_parse(text: str) -> int:
+def scale_parse(text: str) -> Union[int, None]:
     pattern = r"\d/(\d+)"
     scale_text = re.search(pattern, text)
     scale = int(scale_text.group(1)) if scale_text else None
     return scale
 
 
-def size_parse(text: str) -> int:
+def size_parse(text: str) -> Union[int, None]:
     pattern = r"(\d+)[mm|cm|ｍｍ|ｃｍ]"
     is_cm = bool(re.search(r"cm", text))
     size_text = re.search(pattern, text)
