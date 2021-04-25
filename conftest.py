035d5baa2760e1f88d162e71c09fde6b5c21269f
Elton Chou <plscd748@gmail.com>
2021-04-25 18:48:07 +0000

    [CHANGE]stoped sqlalchemy echo

--- conftest.py
+++ conftest.py
@@ -15,7 +15,7 @@ def session():
     POSTGRES_PASSWORD = "try"
     POSTGRES_DB = "figure_testing"
 
-    with db(f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@db:5432/{POSTGRES_DB}", echo=True) as d:
+    with db(f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@db:5432/{POSTGRES_DB}", echo=False) as d:
         engine = d.engine
         session = d.session
 
