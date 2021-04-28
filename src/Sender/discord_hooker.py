7686db7b45cb0faeeab916743b5e8b5c3b7a3236
Elton Chou <plscd748@gmail.com>
2021-04-28 17:52:01 +0000

    [ADD]added DiscordHooker stats

--- src/Sender/discord_hooker.py
+++ src/Sender/discord_hooker.py
@@ -1,29 +1,60 @@
 from typing import Any
 from discord import Embed, Webhook
 from discord.errors import HTTPException
+from src.constants import ProcessorStatus
+from datetime import datetime
 
 
 class DiscordHooker:
+    batch_size = 10
+
     webhooks: list[Webhook]
     embeds_batches: list[list[Embed]]
     webhook_status: dict[str, bool]
+    stats: dict[str, Any]
+    status: ProcessorStatus
 
     def __init__(self, webhooks: list[Webhook], embeds: list[Embed]) -> None:
+        embeds_count = len(embeds)
         self.webhooks = webhooks
-        self.embeds_batches = process_embeds(embeds)
+        self.embeds_batches = process_embeds(embeds, batch_size=self.batch_size)
         self.stats = {}
+        embed_batch_count = len(self.embeds_batches)
+        self._update_stats("embed_count", embeds_count)
+        self._update_stats("embed_batch_size", self.batch_size)
+        self._update_stats("embed_batch_count", embed_batch_count)
+        self._update_stats("webhook_count", len(webhooks))
+        self._update_stats("webhook_sending_count", 0)
+        self._update_stats("webhook_sending_count/success", 0)
+        self._update_stats("webhook_sending_count/failed", 0)
         self.webhook_status = {}
+        self.status = ProcessorStatus.FAILED
 
     def send(self):
+        self._update_stats("start_time", datetime.utcnow())
         for webhook in self.webhooks:
             is_successed = True
             for embeds_batch in self.embeds_batches:
                 try:
+                    self._stats_plusone("webhook_sending_count")
                     webhook.send(embeds=embeds_batch)
+                    self._stats_plusone("webhook_sending_count/success")
                 except HTTPException:
                     is_successed = False
+                    self._stats_plusone("webhook_sending_count/failed")
 
             self.webhook_status[str(webhook.id)] = is_successed
+
+        self.status = ProcessorStatus.SUCCESS
+        self._update_stats("finish_time", datetime.utcnow())
+
+    def _update_stats(self, key, value):
+        self.stats[key] = value
+
+    def _stats_plusone(self, key):
+        stats_value = self.stats[key]
+        if isinstance(stats_value, int):
+            self.stats[key] += 1
 
 
 def process_embeds(embeds: list[Any], batch_size: int = 10) -> list[list[Any]]:
