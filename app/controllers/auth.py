9c813bcad7a316440baf7b185c1553bfbe392d52
Elton Chou <plscd748@gmail.com>
2021-04-28 15:56:18 +0000

    [FIX]fixed webhook creation in "auth.webhook"

--- app/controllers/auth.py
+++ app/controllers/auth.py
@@ -29,12 +29,13 @@ def webhook():
     if r.status_code == 200:
         webhook_response = r.json()
         webhook_channel_id = webhook_response["webhook"]["channel_id"]
-        webhook_url = webhook_response["webhook"]["url"]
+        webhook_id = webhook_response["webhook"]["id"]
+        webhook_token = webhook_response["webhook"]["token"]
         the_hook = Webhook.get_by_channel_id(webhook_channel_id)
         if the_hook:
-            the_hook.update(url=webhook_url)
+            the_hook.update(id=webhook_id, token=webhook_token)
         if not the_hook:
-            the_hook = Webhook.create(channel_id=webhook_channel_id, url=webhook_url)
+            the_hook = Webhook.create(channel_id=webhook_channel_id, id=webhook_id, token=webhook_token)
         the_hook.session.commit()
 
     return redirect(url_for("public.home"))
