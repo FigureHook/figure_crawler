c0ad17c55f871959d1677022de4fc8afabbb044a
Elton Chou <plscd748@gmail.com>
2021-05-03 17:13:20 +0000

    [CHANGE]changed pipline for new announced

--- src/Services/crawler/product_crawler/pipelines.py
+++ src/Services/crawler/product_crawler/pipelines.py
@@ -3,8 +3,11 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
-from typing import Union
 import logging
+from datetime import date
+from typing import Union
+
+from scrapy.spiders.crawl import CrawlSpider
 
 from src.database import pgsql_session
 from src.Factory.model_factory import ProductModelFactory
@@ -13,7 +16,13 @@ from src.Models import Product
 
 
 class SaveProductInDatabasePipeline:
-    def process_item(self, item: product_dataclass, spider):
+    def process_item(self, item: product_dataclass, spider: CrawlSpider):
+        new_keyword = "recent"
+        if spider.name:
+            if new_keyword in spider.name:
+                last_release = item.release_infos.last()
+                if last_release:
+                    last_release.announced_at = date.today()
         with pgsql_session():
             product: Union[Product, None] = Product.query.filter_by(
                 name=item.name,
