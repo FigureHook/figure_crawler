7ae4a83cfa69a5c3b339cc1282cc8adbdd92d726
Elton Chou <plscd748@gmail.com>
2021-05-04 15:57:29 +0000

    [ADD] added check_new_release in celery task

--- src/Services/celery/app.py
+++ src/Services/celery/app.py
@@ -1,4 +1,5 @@
 import os
+from collections import namedtuple
 from typing import Iterable
 
 from celery import Celery
@@ -13,6 +14,7 @@ from src.utils.announcement_checksum import (AlterChecksum, GSCChecksum,
                                              SiteChecksum)
 from src.utils.data_access import (make_discord_webhooks,
                                    make_newly_release_embeds_after)
+from src.utils.scrapyd_api import schedule_spider
 
 rabbit_user = os.getenv("RABBITMQ_DEFAULT_USER")
 rabbit_pw = os.getenv("RABBITMQ_DEFAULT_PASS")
@@ -34,8 +36,13 @@ app.config_from_object(Config)
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(
-        crontab(minute="*/5"),
+        crontab(minute="*/10"),
         news_push.s(),
+        name="push new release through discord webhook",
+    )
+    sender.add_periodic_task(
+        crontab(minute="*/30"),
+        check_new_release.s(),
         name="check product every half-hour",
     )
 
@@ -49,11 +56,9 @@ def news_push():
         discord_webhooks = make_discord_webhooks(adapter)
         embeds = make_newly_release_embeds_after(this_task.executed_at)
         this_task.update()
+        hooker = DiscordHooker(discord_webhooks, embeds=embeds)
+        hooker.send()
 
-    hooker = DiscordHooker(discord_webhooks, embeds=embeds)
-    hooker.send()
-
-    with pgsql_session():
         webhooks: Iterable[Webhook] = Webhook.all()
         for webhook in webhooks:
             is_existed = hooker.webhook_status.get(webhook.id)
@@ -61,3 +66,21 @@ def news_push():
                 webhook.update(is_existed=is_existed)
 
     return hooker.stats
+
+
+@app.task
+def check_new_release():
+    scheduled_jobs = []
+    with pgsql_session():
+        CheckingPair = namedtuple("CheckingPair", ["checksum_cls", "spider_name"])
+        sites_to_check = [
+            CheckingPair(AlterChecksum, "alter_recent"),
+            CheckingPair(GSCChecksum, "gsc_recent")
+        ]
+        for checking_pair in sites_to_check:
+            checksum: SiteChecksum = checking_pair.checksum_cls()
+            if checksum.is_changed:
+                response = schedule_spider(checking_pair.spider_name)
+                scheduled_jobs.append(response)
+                checksum.update()
+    return scheduled_jobs
