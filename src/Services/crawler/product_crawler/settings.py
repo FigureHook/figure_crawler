971c00107a33a95714996351501354347f2e673b
Elton Chou <plscd748@gmail.com>
2021-05-05 21:08:03 +0000

    [CHANGE&ADD] scrapy stuff

--- src/Services/crawler/product_crawler/settings.py
+++ src/Services/crawler/product_crawler/settings.py
@@ -9,8 +9,8 @@
 
 BOT_NAME = 'product_crawler'
 
-SPIDER_MODULES = ['src.Services.crawler.product_crawler.spiders']
-NEWSPIDER_MODULE = 'src.Services.crawler.product_crawler.spiders'
+SPIDER_MODULES = ['Services.crawler.product_crawler.spiders']
+NEWSPIDER_MODULE = 'Services.crawler.product_crawler.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
@@ -64,7 +64,7 @@ ROBOTSTXT_OBEY = False
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
     # 'product_crawler.pipelines.ProductDataProcessingPipeline': 300,
-    'src.Services.crawler.product_crawler.pipelines.SaveProductInDatabasePipeline': 400,
+    'Services.crawler.product_crawler.pipelines.SaveProductInDatabasePipeline': 400,
     # 'gsc_crawler.pipelines.OrderPeriodFillingPipeline': 500
 }
 
@@ -105,5 +105,4 @@ PROXY_MODE = 0
 
 
 # logger settings
-LOG_FILE = '/workspace/logs/crawler.log'
 LOG_LEVEL = 'INFO'
