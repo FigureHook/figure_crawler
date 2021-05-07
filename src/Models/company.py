f73dbba5c589318a1a889ff7101fb0da5d475c6e
Elton Chou <plscd748@gmail.com>
2021-05-07 22:35:14 +0000

    [REMOVE]removed unused code

--- src/Models/company.py
+++ src/Models/company.py
@@ -10,10 +10,6 @@ class Company(UniqueMixin, PkModel):
 
     name = Column(String, nullable=False, unique=True)
 
-    # made_products = relationship("Product", primaryjoin="(Product.manufacturer_id == Company.id)", backref="manufacturer")
-    # distributed_products = relationship("Product", primaryjoin="(Product.distributer_id == Company.id)", backref="distributer")
-    # released_products = relationship("Product", primaryjoin="(Product.releaser_id == Company.id)", backref="releaser")
-
     @classmethod
     def unique_hash(cls, name):
         return name
