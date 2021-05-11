57dfdc4dc8058d7cd115de09c0938a3213312863
Elton Chou <plscd748@gmail.com>
2021-05-11 19:41:49 +0000

    [ADD]added DiscordHookerStats dict like class to collect stats

--- src/Sender/discord_hooker.py
+++ src/Sender/discord_hooker.py
@@ -1,3 +1,4 @@
+from collections import UserDict
 from datetime import datetime
 from typing import Any
 
@@ -7,28 +8,103 @@ from discord.errors import HTTPException, NotFound
 from .abcs import Sender
 
 
+class DiscordHookerStats(UserDict):
+    def __init__(self, embed_count: int, embed_batch_size: int, embed_batch_count: int, webhook_count: int) -> None:
+        init_data = {
+            "start_time": None,
+            "finish_time": None,
+            "embed_count": embed_count,
+            "embed_batch_size": embed_batch_size,
+            "embed_batch_count": embed_batch_count,
+            "webhook_count": webhook_count,
+            "webhook_sending_count": 0,
+            "webhook_sending_count/success": 0,
+            "webhook_sending_count/failed": 0,
+            "webhook_sending_count/404": 0,
+        }
+        super().__init__(init_data)
+
+    @property
+    def embed_count(self):
+        return self.data["embed_count"]
+
+    @property
+    def embed_batch_size(self):
+        return self.data["embed_batch_size"]
+
+    @property
+    def embed_batch_count(self):
+        return self.data["embed_batch_count"]
+
+    @property
+    def webhook_count(self):
+        return self.data["webhook_count"]
+
+    @property
+    def sending_count(self):
+        return self.data["webhook_sending_count"]
+
+    @property
+    def sending_success_count(self):
+        return self.data["webhook_sending_count/success"]
+
+    @property
+    def sending_failed_count(self):
+        return self.data["webhook_sending_count/failed"]
+
+    @property
+    def sending_404_count(self):
+        return self.data["webhook_sending_count/404"]
+
+    @property
+    def start_time(self):
+        return self.data["start_time"]
+
+    @start_time.setter
+    def start_time(self, time: datetime):
+        if not self.start_time:
+            self.data["start_time"] = time
+
+    @property
+    def finish_time(self):
+        return self.data["finish_time"]
+
+    @finish_time.setter
+    def finish_time(self, time: datetime):
+        self.data["finish_time"] = time
+
+    def _sending_count_plusone(self):
+        self.data["webhook_sending_count"] += 1
+
+    def sending_success(self):
+        self._sending_count_plusone()
+        self.data["webhook_sending_count/success"] += 1
+
+    def sending_failed(self):
+        self._sending_count_plusone()
+        self.data["webhook_sending_count/failed"] += 1
+
+    def sending_404(self):
+        self._sending_count_plusone()
+        self.data["webhook_sending_count/404"] += 1
+
+
 class DiscordHooker(Sender):
     batch_size = 10
 
     webhooks: list[Webhook]
     embeds_batches: list[list[Embed]]
     webhook_status: dict[str, bool]
-    _stats: dict[str, Any]
+    _stats: DiscordHookerStats
 
-    def __init__(self, webhooks: list[Webhook], embeds: list[Embed]) -> None:
+    def __init__(self, webhooks: list[Webhook], embeds: list[Embed], stats: DiscordHookerStats = None) -> None:
         self.embeds_count = len(embeds)
-        self.webhooks = webhooks
         self.embeds_batches = process_embeds(embeds, batch_size=self.batch_size)
-        self._stats = {}
-        embed_batch_count = len(self.embeds_batches)
-        self._update_stats("embed_count", self.embeds_count)
-        self._update_stats("embed_batch_size", self.batch_size)
-        self._update_stats("embed_batch_count", embed_batch_count)
-        self._update_stats("webhook_count", len(webhooks))
-        self._update_stats("webhook_sending_count", 0)
-        self._update_stats("webhook_sending_count/success", 0)
-        self._update_stats("webhook_sending_count/failed", 0)
-        self._update_stats("webhook_sending_count/404", 0)
+        if stats:
+            self._stats = stats
+        if not stats:
+            self._stats = DiscordHookerStats(len(embeds), self.batch_size, len(self.embeds_batches), len(webhooks))
+        self.webhooks = webhooks
         self.webhook_status = {}
 
     @property
@@ -36,7 +112,7 @@ class DiscordHooker(Sender):
         return self._stats
 
     def send(self):
-        self._update_stats("start_time", datetime.utcnow())
+        self.stats.start_time = datetime.utcnow()
         if not self.embeds_count:
             return
         for embeds_batch in self.embeds_batches:
@@ -49,32 +125,23 @@ class DiscordHooker(Sender):
 
                 self.webhook_status[str(webhook.id)] = any(webhook_status)
 
-        self._update_stats("finish_time", datetime.utcnow())
+        self.stats.finish_time = datetime.utcnow()
 
     def _send(self, webhook: Webhook, embeds: list[Embed]):
         try:
-            self._stats_plusone("webhook_sending_count")
             webhook.send(embeds=embeds)
-            self._stats_plusone("webhook_sending_count/success")
+            self._stats.sending_success()
         except NotFound:
-            self._stats_plusone("webhook_sending_count/404")
+            self._stats.sending_404()
             return False
         except HTTPException:
-            self._stats_plusone("webhook_sending_count/failed")
+            self._stats.sending_failed()
         except Exception as e:
             # FIXME: how to log the error?
             print(e)
-            self._stats_plusone("webhook_sending_count/failed")
+            self._stats.sending_failed()
         finally:
             return True
-
-    def _update_stats(self, key, value):
-        self.stats[key] = value
-
-    def _stats_plusone(self, key):
-        stats_value = self.stats.get(key)
-        if isinstance(stats_value, int):
-            self.stats[key] += 1
 
 
 def process_embeds(embeds: list[Any], batch_size: int = 10) -> list[list[Any]]:
--- tests/integrated_test/test_discord_hooker.py
+++ tests/integrated_test/test_discord_hooker.py
@@ -1,8 +1,11 @@
-from Sender.discord_hooker import process_embeds, DiscordHooker
-from discord import Webhook, RequestsWebhookAdapter
-from discord.embeds import Embed
+from datetime import datetime
 
+from discord import RequestsWebhookAdapter, Webhook
+from discord.embeds import Embed
 from pytest_mock import MockerFixture
+
+from Sender.discord_hooker import (DiscordHooker, DiscordHookerStats,
+                                   process_embeds)
 
 
 def test_embeds_processor():
@@ -24,3 +27,22 @@ def test_hooker_sending(mocker: MockerFixture):
     hooker = DiscordHooker(webhooks, embeds=embeds)
     hooker.send()
     assert mock_send.call_count == 4
+
+
+def test_hooker_stats():
+    stats = DiscordHookerStats(10, 10, 1, 100)
+    start_time = datetime.now()
+    stats.start_time = start_time
+    stats.sending_success()
+    assert stats.sending_success_count == 1
+    stats.start_time = datetime.now()
+    stats.sending_failed()
+    stats.finish_time = datetime.now()
+    assert stats.sending_failed_count == 1
+    stats.sending_404()
+    finish_time = datetime.now()
+    stats.finish_time = finish_time
+    assert stats.sending_404_count == 1
+    assert stats.sending_count == 3
+    assert stats.start_time == start_time
+    assert stats.finish_time == finish_time
