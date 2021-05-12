1517362eea5f574f947fa510a499e393bc4fd1dc
Elton Chou <plscd748@gmail.com>
2021-05-12 19:24:56 +0000

    [CHANGE] forgot the product.url

--- src/Helpers/db_helper.py
+++ src/Helpers/db_helper.py
@@ -21,7 +21,7 @@ class ReleaseHelper:
 
         return list of Row with
 
-        `_fields('name', 'is_adult', 'series', 'maker', 'price', 'release_date')`
+        `_fields('name', 'url', 'is_adult', 'series', 'maker', 'price', 'release_date')`
         """
         r = select(
             ProductReleaseInfo.product_id,
@@ -35,6 +35,7 @@ class ReleaseHelper:
 
         stmt = select(
             Product.name.label("name"),
+            Product.url.label("url"),
             Product.adult.label("is_adult"),
             Series.name.label("series"),
             Company.name.label("maker"),
@@ -68,56 +69,3 @@ class DiscordHelper:
             discord_webhook = DiscordWebhookAdapter(webhook, webhook_adapter)
             discord_webhooks.append(discord_webhook)
         return discord_webhooks
-
-    @staticmethod
-    @ensure_session
-    def make_new_release_embeds_after(time: datetime):
-        ProductRelease = namedtuple(
-            "ProductRelease",
-            [
-                "name",
-                "url",
-                "maker",
-                "series",
-                "price",
-                "release_date",
-                "image_url",
-            ]
-        )
-        new_releases = Product.query.\
-            join(Product.series).\
-            join(Product.manufacturer).\
-            join(ProductReleaseInfo).\
-            join(Product.official_images).\
-            filter(
-                ProductReleaseInfo.created_at > time,
-                ProductOfficialImage.order == 1,
-                # ensure fetched data parsed by new release checking parser
-                ProductReleaseInfo.announced_at.isnot(None)
-            ).with_entities(
-                Product.name,
-                Product.url,
-                Company.name,
-                Series.name,
-                ProductReleaseInfo.price,
-                ProductReleaseInfo.initial_release_date,
-                ProductOfficialImage.url
-            ).all()
-
-        embeds: list[Embed] = []
-        for release in new_releases:
-            product = ProductRelease(*release)
-            release_date_text = product.release_date.strftime("%Y年%m月")
-            embed = Embed(title=product.name, type="rich", url=product.url, colour=Colour.red())
-            embed.set_image(url=product.image_url)
-            embed.add_field(
-                name="製造商", value=product.maker, inline=False
-            ).add_field(
-                name="系列", value=product.series, inline=False
-            ).add_field(
-                name="價格", value=f"JPY {product.price}", inline=True
-            ).add_field(
-                name="發售時間", value=release_date_text, inline=True
-            )
-            embeds.append(embed)
-        return embeds
