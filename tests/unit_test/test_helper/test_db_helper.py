6880ec21b466b9dfb10e6346a2bdfbaca9438bd6
Elton Chou <plscd748@gmail.com>
2021-05-12 15:01:20 +0000

    [ADD] added test for new release helper, but need to do more test case

--- tests/unit_test/test_helper/test_db_helper.py
+++ tests/unit_test/test_helper/test_db_helper.py
@@ -1,7 +1,7 @@
 import pytest
 from discord import RequestsWebhookAdapter, Webhook
 
-from Helpers.db_helper import DiscordHelper
+from Helpers.db_helper import DiscordHelper, ReleaseHelper
 
 
 @pytest.mark.usefixtures("session")
@@ -11,3 +11,8 @@ class TestDiscorHelper:
         webhooks = DiscordHelper.make_discord_webhooks(adapter)
         for webhook in webhooks:
             assert isinstance(webhook, Webhook)
+
+
+@pytest.mark.usefixtures("session")
+def test_release_helper(session):
+    ReleaseHelper.fetch_new_releases(session)
