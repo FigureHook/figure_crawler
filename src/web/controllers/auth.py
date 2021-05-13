0078e68d5f1c52559d7f3404c9944635614ce769
Elton Chou <plscd748@gmail.com>
2021-05-13 22:27:59 +0000

    [ADD]check state when oauth

--- src/web/controllers/auth.py
+++ src/web/controllers/auth.py
@@ -2,6 +2,7 @@ import os
 
 import requests as rq
 from flask import Blueprint, flash, request
+from flask.globals import session
 from flask.helpers import url_for
 from werkzeug.utils import redirect
 
@@ -27,12 +28,12 @@ def exchange_token(code):
     return r
 
 
-def save_webhook_info(channel_id, _id, token):
+def save_webhook_info(channel_id, _id, token, **kwargs):
     the_hook = Webhook.get_by_channel_id(channel_id)
     if the_hook:
-        the_hook.update(id=_id, token=token)
+        the_hook.update(id=_id, token=token, **kwargs)
     if not the_hook:
-        the_hook = Webhook.create(channel_id=channel_id, id=_id, token=token)
+        the_hook = Webhook.create(channel_id=channel_id, id=_id, token=token, **kwargs)
     the_hook.session.commit()
 
 
@@ -41,13 +42,15 @@ def webhook():
     args = request.args.to_dict()
 
     r = exchange_token(args["code"])
+    state = args["state"]
 
-    if r.status_code == 200:
+    if r.status_code == 200 and session['state'] == state:
         webhook_response = r.json()
         webhook_channel_id = webhook_response["webhook"]["channel_id"]
         webhook_id = webhook_response["webhook"]["id"]
         webhook_token = webhook_response["webhook"]["token"]
-        save_webhook_info(webhook_channel_id, webhook_id, webhook_token)
+        webhook_setting = session['webhook_setting']
+        save_webhook_info(webhook_channel_id, webhook_id, webhook_token, **webhook_setting)
         flash("Hooking success!")
     else:
         flash("Webhook authorization failed.")
--- src/web/controllers/public.py
+++ src/web/controllers/public.py
@@ -1,17 +1,22 @@
 import os
+from base64 import b64encode
+from os import urandom
 
-from flask import Blueprint, flash, redirect, render_template, request, url_for
+from flask import (Blueprint, flash, redirect, render_template, request,
+                   session, url_for)
 
 from web.forms import SubscriptionForm
 
 blueprint = Blueprint("public", __name__)
 
 
-def discord_redirect_url():
+def discord_redirect_url(state):
     base_uri = "https://discord.com/api/oauth2/authorize"
     redirect_uri = url_for('auth.webhook', _external=True)
     client_id = os.getenv('DISCORD_CLIENT_ID')
-    return f"{base_uri}?response_type=code&client_id={client_id}&scope=webhook.incoming&redirect_uri={redirect_uri}"
+    return "{}?response_type=code&client_id={}&scope=webhook.incoming&redirect_uri={}&state={}".format(
+        base_uri, client_id, redirect_uri, state
+    )
 
 
 @blueprint.route("/", methods=('GET', 'POST'))  # type: ignore
@@ -23,7 +28,15 @@ def home():
 
     if request.method == 'POST':
         if form.validate_on_submit():
-            location = discord_redirect_url()
+            state = b64encode(urandom(12)).decode('utf-8')
+            session['state'] = state
+            location = discord_redirect_url(state)
+
+            session['webhook_setting'] = {
+                'is_nsfw': form.is_nsfw.data,
+                'lang': form.language.data
+            }
+
             return redirect(location)
 
         flash("wtf are you doing?")
