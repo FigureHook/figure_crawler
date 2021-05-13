92e6d80703566505fe72155dabc26988e8f651eb
Elton Chou <plscd748@gmail.com>
2021-05-13 14:12:41 +0000

    [CHANGE] changed web subscription form

--- src/web/forms.py
+++ src/web/forms.py
@@ -1,7 +1,17 @@
 import os
 
 from flask_wtf import FlaskForm
-from wtforms import HiddenField
+from wtforms import BooleanField, HiddenField, SelectField, ValidationError
+
+from Helpers.general_helper import DataHelper
+
+
+def validate_language(form, field: SelectField):
+    supporting_langs = DataHelper.webhook_supporting_languages()
+
+    if field.data not in supporting_langs:
+        raise ValidationError(
+            f"language: {field.data} is not supported now.\nCurrently supported language: {supporting_langs}")
 
 
 class SubscriptionForm(FlaskForm):
@@ -9,3 +19,13 @@ class SubscriptionForm(FlaskForm):
     client_id = HiddenField(default=os.getenv("DISCORD_CLIENT_ID"))
     scope = HiddenField(default="webhook.incoming")
     redirect_uri = HiddenField()
+    is_nsfw = BooleanField("nsfw")
+    language = SelectField(
+        "language",
+        choices=[
+            ('zh-TW', '繁體中文'),
+            ('en', 'English'),
+            ('ja', '日本語')
+        ],
+        validators=[validate_language]
+    )
