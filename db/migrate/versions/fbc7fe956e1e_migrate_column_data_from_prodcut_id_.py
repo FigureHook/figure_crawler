b5cc0ad994f39c131e46ab79c5796b99ee7a25e7
Elton Chou <plscd748@gmail.com>
2021-05-16 04:45:02 +0000

    [FIX]fixed column typo, what a mess...

---
+++ db/migrate/versions/fbc7fe956e1e_migrate_column_data_from_prodcut_id_.py
@@ -0,0 +1,59 @@
+"""migrate column data from prodcut_id -> product_id
+
+Revision ID: fbc7fe956e1e
+Revises: dc7042a9ce99
+Create Date: 2021-05-15 08:26:53.552511
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+from Models.relation_table import (product_paintwork_table,
+                                   product_sculptor_table)
+
+# revision identifiers, used by Alembic.
+revision = 'fbc7fe956e1e'
+down_revision = 'dc7042a9ce99'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    bind = op.get_bind()
+
+    tables = [
+        'product_paintwork',
+        'product_sculptor'
+    ]
+
+    for t in tables:
+        pids = set()
+        for r in bind.execute(sa.text(f"SELECT * FROM {t}")):
+            pids.add(r['prodcut_id'])
+
+        for pid in pids:
+            bind.execute(
+                sa.text(
+                    f"UPDATE {t} SET product_id={pid} WHERE {t}.prodcut_id={pid}"
+                )
+            )
+
+
+def downgrade():
+    bind = op.get_bind()
+    tables = [
+        'product_paintwork',
+        'product_sculptor'
+    ]
+
+    for t in tables:
+        pids = set()
+        for r in bind.execute(sa.text(f"SELECT * FROM {t}")):
+            pids.add(r['product_id'])
+
+        for pid in pids:
+            bind.execute(
+                sa.text(
+                    f"UPDATE {t} SET prodcut_id={pid} WHERE {t}.product_id={pid}"
+                )
+            )
--- src/Models/relation_table.py
+++ src/Models/relation_table.py
@@ -6,12 +6,12 @@ metadata = Model.metadata
 # association table
 product_sculptor_table = Table(
     "product_sculptor", metadata,
-    Column("prodcut_id", Integer, ForeignKey("product.id")),
+    Column("product_id", Integer, ForeignKey("product.id")),
     Column("sculptor_id", Integer, ForeignKey("sculptor.id"))
 )
 
 product_paintwork_table = Table(
     "product_paintwork", metadata,
-    Column("prodcut_id", Integer, ForeignKey("product.id")),
+    Column("product_id", Integer, ForeignKey("product.id")),
     Column("paintwork_id", Integer, ForeignKey("paintwork.id"))
 )
