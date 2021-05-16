539d8dbd3951afcea113a8655477c7bd86782fb4
Elton Chou <plscd748@gmail.com>
2021-05-16 05:04:18 +0000

    [CHANGE]cascade delete worker relationship

--- src/Models/relation_table.py
+++ src/Models/relation_table.py
@@ -6,12 +6,12 @@ metadata = Model.metadata
 # association table
 product_sculptor_table = Table(
     "product_sculptor", metadata,
-    Column("product_id", Integer, ForeignKey("product.id")),
-    Column("sculptor_id", Integer, ForeignKey("sculptor.id"))
+    Column("product_id", Integer, ForeignKey("product.id", ondelete="CASCADE")),
+    Column("sculptor_id", Integer, ForeignKey("sculptor.id", ondelete="CASCADE"))
 )
 
 product_paintwork_table = Table(
     "product_paintwork", metadata,
-    Column("product_id", Integer, ForeignKey("product.id")),
-    Column("paintwork_id", Integer, ForeignKey("paintwork.id"))
+    Column("product_id", Integer, ForeignKey("product.id", ondelete="CASCADE")),
+    Column("paintwork_id", Integer, ForeignKey("paintwork.id", ondelete="CASCADE"))
 )
