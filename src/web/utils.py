e3e3c25a56255280ec7f426bdad6cf07b3046099
Elton Chou <plscd748@gmail.com>
2021-05-20 18:49:50 +0000

    [FIX] strip string

--- src/web/utils.py
+++ src/web/utils.py
@@ -1,4 +1,4 @@
 def get_maintenance_time():
     with open('/flags/maintenance.on', 'r') as f:
-        retry_after = f.readline()
+        retry_after = f.readline().strip()
     return retry_after
