d52b9e9509a1a8bcf2d2d5d89a3be617ea1d2eca
Elton Chou <plscd748@gmail.com>
2021-06-08 12:23:14 +0000

    [REFINE]refined dispatcher cache

--- libs/figure_hook/src/figure_hook/Dispatchers/discord_hook_dispatcher.py
+++ libs/figure_hook/src/figure_hook/Dispatchers/discord_hook_dispatcher.py
@@ -22,24 +22,27 @@ class DiscordNewReleaseEmbedsDispatcher:
 
     def dispatch(self):
         for webhook in self.webhooks:
-            cache_key = cache_key = (webhook.lang, webhook.is_nsfw)
+            cache_key = (webhook.lang, webhook.is_nsfw)
             discord_webhook = DiscordWebhookAdapter(webhook, self.adapter)
             embeds = self._get_embeds_from_cache(cache_key)
             self.hooker.send(discord_webhook, embeds)
 
-    def _get_embeds_from_cache(self, key):
-        embeds = self.embeds_cache.get(key, [])
-        if not embeds:
-            webhook_lang = key[0]
-            webhook_is_nsfw = key[1]
-            for e in self.raw_embeds:
-                if webhook_is_nsfw:
-                    e.localized_with(webhook_lang)
-                    embeds.append(e)
-                else:
-                    if not e.is_nsfw:
-                        e.localized_with(webhook_lang)
-                        embeds.append(e)
+    def _get_embeds_from_cache(self, key: tuple[str, bool]):
+        webhook_lang, webhook_is_nsfw = key
+        embeds = self.embeds_cache.get(key)
+
+        if embeds is None:
+            # prevent running loop when embeds is []
+            embeds = []
+            for raw_embed in self.raw_embeds:
+                embed_should_be_processed = any((
+                    webhook_is_nsfw,
+                    not webhook_is_nsfw and not raw_embed.is_nsfw
+                ))
+
+                if embed_should_be_processed:
+                    embed = raw_embed.localized_with(webhook_lang)
+                    embeds.append(embed)
 
             self.embeds_cache.setdefault(key, embeds)
 
