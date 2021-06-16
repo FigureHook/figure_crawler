2d1574777d6e6a9b1a4ce277cd8ab5577c062849
Elton Chou <plscd748@gmail.com>
2021-06-16 16:17:21 +0000

    [ADD]add release_helper test but lack of test

--- tests/unit_test/test_helper/test_db_helper.py
+++ tests/unit_test/test_helper/test_db_helper.py
@@ -1,10 +1,13 @@
 from datetime import datetime
 
 import pytest
+from figure_hook.extension_class import ReleaseFeed
 from figure_hook.Helpers.db_helper import ReleaseHelper
 
 
 @pytest.mark.usefixtures("session")
 def test_release_helper(session):
     # TODO:more test case
-    ReleaseHelper.fetch_new_releases(session, datetime(2021, 5, 1))
+    release_feeds = ReleaseHelper.fetch_new_releases(session, datetime(2021, 5, 1))
+    for rf in release_feeds:
+        assert isinstance(rf, ReleaseFeed)
