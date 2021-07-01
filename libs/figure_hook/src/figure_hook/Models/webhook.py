ad23d632f16c342784af7b8af925eccc139cbb7d
Elton Chou <plscd748@gmail.com>
2021-07-01 18:43:41 +0000

    [REFINE]refined import

--- libs/figure_hook/src/figure_hook/Models/webhook.py
+++ libs/figure_hook/src/figure_hook/Models/webhook.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Boolean, Column, String
 from sqlalchemy.orm import validates
-from sqlalchemy_mixins import TimestampsMixin
+from sqlalchemy_mixins.timestamp import TimestampsMixin
 
 from .base import Model
 
