e82a890b82e1aa8d6cc603b60211e08caebb8bec
Elton Chou <plscd748@gmail.com>
2021-10-30 19:36:26 +0000

    fixed task test workflow

--- tests/integrated_test/test_task.py
+++ tests/integrated_test/test_task.py
@@ -8,35 +8,41 @@ from figure_hook.Publishers.abcs import Stats
 from figure_hook.Tasks.on_demand import send_discord_welcome_webhook
 from figure_hook.Tasks.periodic import (DiscordNewReleasePush, NewReleasePush,
                                         PlurkNewReleasePush)
+from figure_hook.database import pgsql_session
 
 
 def test_send_welcome_hook(mocker: MockerFixture):
-    webhook_send = mocker.patch("discord.webhook.Webhook.send", return_value=True)
+    webhook_send = mocker.patch(
+        "discord.webhook.Webhook.send", return_value=True)
     sending_stats = send_discord_welcome_webhook(123, "token", "Hello!")
     assert isinstance(sending_stats, Stats)
     assert webhook_send.call_count == 1
 
 
-@pytest.mark.usefixtures("session")
+@pytest.mark.usefixtures("fake_data")
 class NewsReleasePush(ABC):
     task_cls: Type[NewReleasePush]
 
-    @pytest.fixture
+    @pytest.fixture()
     def mock_publisher(self, mocker: MockerFixture):
         mocker.patch('plurk_oauth.PlurkAPI.callAPI', return_value={"a": True})
         mocker.patch('discord.webhook.Webhook.send')
 
     @pytest.mark.usefixtures("mock_publisher")
     def test_attributes(self):
-        task = self.task_cls()
-        assert hasattr(task, 'name')
-        assert type(task.name) is str
+        with pgsql_session() as session:
+            task = self.task_cls(session)
+            assert type(task.name) is str
+            assert task.session is session
 
     @pytest.mark.usefixtures("mock_publisher")
     def test_execution(self):
-        task = self.task_cls()
-        result = task.execute()
-        assert isinstance(result, Stats)
+        with pgsql_session() as session:
+            task = self.task_cls(session)
+            last_executed_at = task.executed_at
+            result = task.execute()
+            assert isinstance(result, Stats)
+            assert last_executed_at is not task.executed_at
 
 
 class TestDiscordNewsReleasePush(NewsReleasePush):
