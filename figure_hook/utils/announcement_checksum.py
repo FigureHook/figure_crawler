33a44a18d3cd683e05829d8684bf2ef9c29ebefb
Elton Chou <plscd748@gmail.com>
2021-11-02 02:06:41 +0800

    fixed SiteChecksum
    SiteChecksum shouldn't depends on ScrapydUtil directly,
    better follow DIP

--- figure_hook/utils/announcement_checksum.py
+++ figure_hook/utils/announcement_checksum.py
@@ -3,14 +3,15 @@ from hashlib import md5
 from typing import Optional
 
 import requests as rq
-from figure_hook.constants import SourceSite
-from figure_hook.Helpers.datetime_helper import DatetimeHelper
-from figure_hook.Models import AnnouncementChecksum
 from figure_parser.alter.announcecment_parser import fetch_alter_newest_year
 from figure_parser.constants import AlterCategory, GSCCategory, GSCLang
 from figure_parser.utils import RelativeUrl
 
-from .scrapyd_api import schedule_spider
+from figure_hook.constants import SourceSite
+from figure_hook.Helpers.datetime_helper import DatetimeHelper
+from figure_hook.Models import AnnouncementChecksum
+
+from .scrapyd_api import ScrapydUtil
 
 __all__ = [
     "GSCChecksum",
@@ -28,8 +29,9 @@ class SiteChecksum(ABC):
     __site__: SourceSite
     __spiders__: list[str]
     __site_checksum: Optional[AnnouncementChecksum]
+    scrapyd_util: ScrapydUtil
 
-    def __init__(self) -> None:
+    def __init__(self, scrapyd_util: ScrapydUtil) -> None:
         if not hasattr(self, "__site__"):
             raise NotImplementedError(
                 "Class attribute `__site__` should be implemented."
@@ -37,6 +39,7 @@ class SiteChecksum(ABC):
 
         self.__site_checksum = AnnouncementChecksum.get_by_site(self.__site__)
         self._feature = self._extract_feature()
+        self.scrapyd_util = scrapyd_util
 
     @property
     def feature(self):
@@ -69,12 +72,11 @@ class SiteChecksum(ABC):
                 checksum=self.current
             )
 
-    @classmethod
-    def trigger_crawler(cls) -> list:
+    def trigger_crawler(self) -> list:
         """Trigger the spiders to parse new product."""
         jobs = []
-        for spider in cls.__spiders__:
-            job = schedule_spider(spider)
+        for spider in self.__spiders__:
+            job = self.scrapyd_util.schedule_spider(spider)
             jobs.append(job)
 
         return jobs
