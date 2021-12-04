e332eca58a13bcc3fd502dcc1d0c3de9b9659806
Elton H.Y. Chou <plscd748@gmail.com>
2021-12-05 02:57:23 +0800

    Fix #13

--- figure_hook/Factory/publish_factory/plurk_content_factory.py
+++ figure_hook/Factory/publish_factory/plurk_content_factory.py
@@ -4,6 +4,18 @@ from figure_hook.extension_class import ReleaseFeed
 from .abcs import PublishFactory
 
 
+def link(text: str, url: str) -> str:
+    return f"{url} ({text})"
+
+
+def bold(text) -> str:
+    return f"**{text}**"
+
+
+def italic(text: str) -> str:
+    return f"*{text}*"
+
+
 class PlurkContentFactory(PublishFactory):
     @staticmethod
     def create_new_release(release_feed: ReleaseFeed):
@@ -18,17 +30,22 @@ class PlurkContentFactory(PublishFactory):
 
         category_text = "再販" if release_feed.resale else "新品"
 
+        post_category = bold(f"[{category_text}速報]")
+        post_product_name = link(release_feed.name, release_feed.url)
+
+        figure_hook_link = link("Discord 速報訂閱", "https://bit.ly/3wj8Gpj")
+
         content = "" \
-            f"[**{category_text}速報**]\n" \
-            f"商品名: [{release_feed.name}]({release_feed.url})\n" \
+            f"{post_category}\n" \
+            f"商品名: {post_product_name}\n" \
             f"作品名稱: {release_feed.series}\n" \
             f"製造商: {release_feed.maker}\n" \
-            f"尺寸: {release_feed.size}mm\n" \
+            f"尺寸: {release_feed.size}mm(H)\n" \
             f"發售日期: {release_date_text}\n" \
             f"價格: {price_text}\n" \
             f"{release_feed.media_image}\n" \
             "----------\n" \
-            "📨 [Discord 速報訂閱](https://bit.ly/3wj8Gpj)"
+            f"📨 {figure_hook_link}"
 
         return {
             "content": content,
